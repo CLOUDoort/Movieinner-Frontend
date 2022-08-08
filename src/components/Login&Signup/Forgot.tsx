a379c6ad3b2f3ef46aebc0d60ecc234c3a48ddf7
JunSeok <ppu1234@naver.com>
2022-08-08 15:25:48 +0900

    0808_forgot

--- src/components/Login&Signup/Forgot.tsx
+++ src/components/Login&Signup/Forgot.tsx
@@ -1,5 +1,29 @@
+import { useState } from 'react'
+import { EmailForm, SignupContainerDiv } from './Signup.style'
+
 const Forgot = () => {
-    return <></>
+    const [email, setEmail] = useState('')
+
+    const handleChange = (e) => {
+        e.preventDefault()
+        const { value } = e.target
+        setEmail(value)
+    }
+    const handleSubmit = (e) => {
+        e.preventDefault()
+    }
+    return (
+        <>
+            <SignupContainerDiv>
+                <div>비밀번호 찾기</div>
+                <EmailForm onSubmit={handleSubmit}>
+                    <div>Email</div>
+                    <input type='email' name='email' value={email} placeholder='이메일을 입력해주세요' onChange={handleChange} />
+                    <button>인증 메일 발송!</button>
+                </EmailForm>
+            </SignupContainerDiv>
+        </>
+    )
 }
 
 export default Forgot
---
+++ src/components/Login&Signup/Forgotvalid.tsx
@@ -0,0 +1,31 @@
+import { useState } from 'react'
+import { EmailForm, ProgressBtn, SignupContainerDiv } from './Signup.style'
+
+const Forgotvalid = () => {
+    const [email, setEmail] = useState('')
+
+    const handleChange = (e) => {
+        e.preventDefault()
+        const { value } = e.target
+        setEmail(value)
+    }
+    const handleSubmit = (e) => {
+        e.preventDefault()
+    }
+    return (
+        <>
+            <SignupContainerDiv>
+                <div>비밀번호 찾기</div>
+                <EmailForm onSubmit={handleSubmit}>
+                    <input type='email' name='email' value={email} placeholder='새 비밀번호를 입력하세요' onChange={handleChange} />
+                    <input type='email' name='email' value={email} placeholder='새 비밀번호를 다시 입력하세요' onChange={handleChange} />
+                </EmailForm>
+                <div>
+                    <ProgressBtn>완료</ProgressBtn>
+                </div>
+            </SignupContainerDiv>
+        </>
+    )
+}
+
+export default Forgotvalid
