e9f57b27d93eae55f8a3fea7bbccf45969bf5da9
JunSeok <ppu1234@naver.com>
2022-08-22 16:52:23 +0900

    password UI fixed

--- src/components/Auth/Signup/Signuppw.tsx
+++ src/components/Auth/Signup/Signuppw.tsx
@@ -1,4 +1,4 @@
-import { useState } from 'react'
+import { useMemo, useState } from 'react'
 import { useDispatch } from 'react-redux'
 import { setComponent, setUser } from '../../../store/reducers/signupSlice'
 import { EmailDiv, GreenText, ProgressBtn, RedText, SignupContainerDiv } from './Signup_pw.style'
@@ -10,10 +10,21 @@ const Signuppw = () => {
         first: '',
         second: '',
     })
+    const [touchedPw, setTouchedPw] = useState({
+        first: false,
+        second: false,
+    })
 
     const handleChange = (e) => {
         const { name, value } = e.target
         setPassword({ ...password, [name]: value })
+    }
+    const handleFocus = (e) => {
+        const { name } = e.target
+        setTouchedPw({
+            ...touchedPw,
+            [name]: true,
+        })
     }
     const handleClick = (e) => {
         dispatch(setUser({ key: 'password', value: password.first }))
@@ -27,11 +38,29 @@ const Signuppw = () => {
                 <p>비밀번호 설정</p>
                 <EmailDiv>
                     <label>비밀번호 입력</label>
-                    <input type='password' name='first' required value={password.first} onChange={handleChange} placeholder='6자리 이상 입력해 주세요.' />
-                    {password.first.length >= 6 ? <GreenText>알맞은 비밀번호 입니다.</GreenText> : <RedText>아직 6자리가 아니에요.</RedText>}
+                    <input
+                        type='password'
+                        name='first'
+                        required
+                        value={password.first}
+                        onChange={handleChange}
+                        placeholder='6자리 이상 입력해 주세요.'
+                        onFocus={handleFocus}
+                    />
+                    {touchedPw.first === true &&
+                        (password.first.length >= 6 ? <GreenText>알맞은 비밀번호 입니다.</GreenText> : <RedText>아직 6자리가 아니에요.</RedText>)}
                     <label>비밀번호 확인</label>
-                    <input type='password' name='second' required value={password.second} onChange={handleChange} placeholder='다시 한번 입력해 주세요.' />
-                    {password.first !== password.second ? <RedText>두 비밀번호가 달라요&#33;</RedText> : <GreenText>일치합니다.</GreenText>}
+                    <input
+                        type='password'
+                        name='second'
+                        required
+                        value={password.second}
+                        onChange={handleChange}
+                        placeholder='다시 한번 입력해 주세요.'
+                        onFocus={handleFocus}
+                    />
+                    {touchedPw.second === true &&
+                        (password.first !== password.second ? <RedText>두 비밀번호가 달라요&#33;</RedText> : <GreenText>일치합니다.</GreenText>)}
                 </EmailDiv>
                 <ProgressBtn disabled={password.first === '' || password.first !== password.second} onClick={handleClick}>
                     다음
