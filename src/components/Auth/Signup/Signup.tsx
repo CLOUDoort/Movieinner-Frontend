39781b04d5b4283b7ab9af428d918b87d1e5cb0f
JunSeok <ppu1234@naver.com>
2022-08-28 00:21:11 +0900

    fixed : emailRegExp

--- src/components/Auth/Signup/Signup.tsx
+++ src/components/Auth/Signup/Signup.tsx
@@ -1,5 +1,6 @@
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
 import { useDispatch, useSelector } from 'react-redux'
+import { apiInstance } from '../../../apis/setting'
 import { setSignup, setUser } from '../../../store/reducers/signupSlice'
 import { RootState } from '../../../store/store'
 import CurrentStatusFirst from '../CurrentStatus/CurrentStatusFirst'
@@ -7,18 +8,18 @@ import Signupinfo from './Signupinfo'
 import Signuppw from './Signuppw'
 import SignupVerify from './SignupVerify'
 import { EmailDiv, GreenText, ProgressBtn, RedText, SignupContainerDiv } from './Signup_pw.style'
+import { emailRegExp } from '../SignupLogic'
 
 const Signup = () => {
     const signupComponent = useSelector((state: RootState) => state.signup.component)
     const dispatch = useDispatch()
     const [emailValid, setEmailValid] = useState({
+        // 이메일 정규식 확인
         touch: false,
         valid: false,
     })
-
+    const [checkEmail, setCheckEmail] = useState(false) // 이메일 중복 여부
     const [email, setEmail] = useState('')
-    const emailRegExp =
-        /(?:[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*|"(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21\x23-\x5b\x5d-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])*")@(?:(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?|\[(?:(?:(2(5[0-5]|[0-4][0-9])|1[0-9][0-9]|[1-9]?[0-9]))\.){3}(?:(2(5[0-5]|[0-4][0-9])|1[0-9][0-9]|[1-9]?[0-9])|[a-z0-9-]*[a-z0-9]:(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21-\x5a\x53-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])+)\])/
 
     const handleClick = async (e) => {
         e.preventDefault()
@@ -32,15 +33,23 @@ const Signup = () => {
             touch: true,
         })
     }
-
-    const handleChange = (e) => {
+    const handleChange = async (e) => {
         const { value } = e.target
         setEmail(value)
-        if (emailRegExp.test(email)) {
+        if (email.match(emailRegExp)) {
             setEmailValid({
                 ...emailValid,
                 valid: true,
             })
+            try {
+                const response = await apiInstance.post('/users/email', { email: email })
+                if (response.data.isEmailExisted) setCheckEmail(true)
+                else setCheckEmail(false)
+                console.log(response)
+            } catch (e) {
+                console.log(e.response)
+                setCheckEmail(false)
+            }
         } else {
             setEmailValid({
                 ...emailValid,
@@ -68,13 +77,17 @@ const Signup = () => {
                         <div>
                             {emailValid.touch === true &&
                                 (emailValid.valid === true ? (
-                                    <GreenText>올바른 이메일 형식입니다.</GreenText>
+                                    checkEmail === true ? (
+                                        <RedText>존재하는 이메일입니다.</RedText>
+                                    ) : (
+                                        <GreenText>올바른 이메일 형식입니다.</GreenText>
+                                    )
                                 ) : (
                                     <RedText>올바르지 않은 이메일 형식입니다.</RedText>
                                 ))}
                         </div>
                     </EmailDiv>
-                    <ProgressBtn disabled={email === '' || !emailRegExp.test(email)} onClick={handleClick}>
+                    <ProgressBtn disabled={(email === '' || !email.match(emailRegExp)) && checkEmail === true} onClick={handleClick}>
                         다음
                     </ProgressBtn>
                 </SignupContainerDiv>
