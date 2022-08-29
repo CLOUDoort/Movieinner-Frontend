73ba0421c11c6a7b44b4760024ae56e357f2611e
JunSeok <ppu1234@naver.com>
2022-08-29 23:48:37 +0900

    fixed : double-rendering

--- next.config.js
+++ next.config.js
@@ -1,6 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    reactStrictMode: true,
+    reactStrictMode: false,
     swcMinify: true,
     images: {
         domains: ['movie-inner.s3.ap-northeast-2.amazonaws.com'],
