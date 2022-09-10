bc1c9a4f7db34f71d2c4d8615c36d6ad43e41a4a
JunSeok <ppu1234@naver.com>
2022-09-10 23:34:40 +0900

    fixed : googlelogin-token

--- src/components/Auth/Login/NaverLogin.tsx
+++ src/components/Auth/Login/NaverLogin.tsx
@@ -1,15 +1,19 @@
 import axios from 'axios'
+import Router from 'next/router'
 import { useEffect, useState } from 'react'
+import { useDispatch } from 'react-redux'
 import { apiInstance } from '../../../apis/setting'
+import { setToken } from '../../../store/reducers/logintokenSlice'
+import { setSocialEmail } from '../../../store/reducers/socialSlice'
 import Loading from '../../Loading'
 import Signupinfo from '../Signup/Signupinfo'
 
 const NaverLogin = () => {
     const [valid, setValid] = useState(false)
+    const dispatch = useDispatch()
 
     useEffect(() => {
         const NAVER_CODE = new URL(window.location.href).searchParams.get('code')
-        console.log(NAVER_CODE)
         const postCode = async () => {
             try {
                 const response = await apiInstance.post('/auth/naver', null, {
@@ -17,13 +21,28 @@ const NaverLogin = () => {
                         authorizationCode: NAVER_CODE,
                     },
                 })
-                console.log(response)
+                const userEmail = response.data.success.response.email
+                const checkUser = await apiInstance.post('/users/email', { email: userEmail })
+                dispatch(setSocialEmail(userEmail))
+                if (checkUser.data.isEmailExisted) {
+                    // 이미 있는 계정
+                    try {
+                        // 액세스 토큰 받고 홈으로
+                        const tokenResponse = await apiInstance.post('/auth', { email: userEmail })
+                        const { accessToken } = tokenResponse.data
+                        dispatch(setToken(accessToken))
+                        Router.replace('/')
+                    } catch (e) {
+                        console.log(e.response)
+                    }
+                }
+                setValid(true)
             } catch (e) {
                 console.log(e.repsonse)
             }
         }
         postCode()
-    }, [])
+    }, [dispatch])
 
     return <>{valid ? <Signupinfo /> : <Loading />}</>
 }
