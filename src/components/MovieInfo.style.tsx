e2bdd9218788e15600ddfd75cf72a9805e7ff1bf
JunSeok <ppu1234@naver.com>
2022-09-17 15:25:06 +0900

    Add : movieInfo

---
+++ src/components/MovieInfo.style.tsx
@@ -0,0 +1,8 @@
+import styled from '@emotion/styled'
+
+export const MovieInfoContainer = styled.div`
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+`
--- src/components/MovieInfo.tsx
+++ src/components/MovieInfo.tsx
@@ -1,7 +1,19 @@
+import { useEffect } from 'react'
+import { apiInstance } from '../apis/setting'
+import { MovieInfoContainer } from './MovieInfo.style'
+
 const MovieInfo = () => {
+    useEffect(() => {
+        const getMovieInfo = async () => {
+            const response = await apiInstance.get('')
+        }
+        getMovieInfo()
+    }, [])
     return (
         <>
-            <p>Movie Info</p>
+            <MovieInfoContainer>
+                <p>Movie Info</p>
+            </MovieInfoContainer>
         </>
     )
 }
