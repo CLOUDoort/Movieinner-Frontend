caaee2388900ba47403ec42b77d254a15f957feb
JunSeok <ppu1234@naver.com>
2022-09-23 23:25:19 +0900

    Add : movieinfo-actor

--- src/components/MovieInfo.tsx
+++ src/components/MovieInfo.tsx
@@ -17,6 +17,7 @@ interface MovieInfoDataList {
 const MovieInfo = () => {
     const router = useRouter()
     const [movieInfo, setMovieInfo] = useState<MovieInfoDataList | null>({})
+    const [actorInfo, setActorInfo] = useState([])
 
     console.log('movieInfo: ', movieInfo)
     useEffect(() => {
@@ -25,34 +26,35 @@ const MovieInfo = () => {
                 if (!router.isReady) return
                 const { movieId } = router.query
                 console.log('movieId: ', movieId)
-                const response = await apiInstance.get(`/movies/details/${movieId}`)
-                const movieResponse = response.data
+                const movieResponse = await apiInstance.get(`/movies/details/${movieId}`)
+                const movieInfoResponse = movieResponse.data
                 const movieInfoBox = {}
                 const dataList = ['title', 'backdrop_path', 'poster_path', 'overview', 'runtime', 'release_date']
                 dataList.forEach((obj) => {
-                    movieInfoBox[obj] = movieResponse[obj]
+                    movieInfoBox[obj] = movieInfoResponse[obj]
                 })
                 setMovieInfo(movieInfoBox)
+
+                const actorResponse = await apiInstance.get(`movies/credits/${movieId}`)
+                const actorInfoResponse = actorResponse.data
+                setActorInfo(actorInfoResponse)
             } catch (e) {
                 console.error('error: ', e.response)
             }
         }
         getMovieInfo()
     }, [router.isReady, router.query])
+    console.log('actorInfoResponse: ', actorInfo)
     if (!movieInfo.backdrop_path) return
     return (
         <>
             <MovieInfoContainer>
                 <MovieBackdropImgContainer>
-                    <Link target='_blank' rel='noreferrer' href={`https://image.tmdb.org/t/p/w500${movieInfo.backdrop_path}`}>
-                        <a>
-                            <Image src={`https://image.tmdb.org/t/p/w500${movieInfo.backdrop_path}`} alt={movieInfo.title} width={600} height={300} />
-                        </a>
-                    </Link>
+                    <Image src={`https://image.tmdb.org/t/p/w500${movieInfo.backdrop_path}`} alt={movieInfo.title} layout='fill' objectFit='scale-down' />
                 </MovieBackdropImgContainer>
-
                 <div></div>
                 <p>{movieInfo.overview}</p>
+                <p>찜하기</p>
             </MovieInfoContainer>
         </>
     )
