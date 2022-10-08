c3bb59591eec6e54632b2f74bd4a5c3789146a3a
JunSeok <ppu1234@naver.com>
2022-10-09 03:11:55 +0900

    Updated : community-feed

--- src/components/Community/Feed.tsx
+++ src/components/Community/Feed.tsx
@@ -3,11 +3,7 @@ import dynamic from 'next/dynamic'
 const NoSsrWysiwyg = dynamic(() => import('./WysiwygEditor'), { ssr: false })
 
 const Feed = () => {
-    return (
-        <>
-            <Link href='/community/write'>글 작성하기</Link>
-        </>
-    )
+    return <Link href='/community/write'>글 작성하기</Link>
 }
 
 export default Feed
