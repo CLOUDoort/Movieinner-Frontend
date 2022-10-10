f7a83f4a206d74d1dcea372ea95f7399c8c1157f
JunSeok <ppu1234@naver.com>
2022-10-10 19:50:12 +0900

    Updated : community-feed

--- src/components/Community/Feed.tsx
+++ src/components/Community/Feed.tsx
@@ -1,9 +1,21 @@
-import Link from 'next/link'
-import dynamic from 'next/dynamic'
-const NoSsrWysiwyg = dynamic(() => import('./WysiwygEditor'), { ssr: false })
+import { useSelector } from 'react-redux'
+import { RootState } from '../../store/store'
+import { toast } from 'react-toastify'
+import { useRouter } from 'next/router'
 
 const Feed = () => {
-    return <Link href='/community/write'>글 작성하기</Link>
+    const accessToken = useSelector((state: RootState) => state.token.token)
+    const router = useRouter()
+
+    const clickWrite = () => {
+        if (accessToken) {
+            router.push('/community/write')
+        } else {
+            toast.error('로그인이 필요합니다!')
+        }
+    }
+
+    return <button onClick={clickWrite}>글 작성하기</button>
 }
 
 export default Feed
