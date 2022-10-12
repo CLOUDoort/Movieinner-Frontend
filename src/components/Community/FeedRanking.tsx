12ab9440ce10dd21b6bec76ccf45f3dcd770931c
JunSeok <ppu1234@naver.com>
2022-10-12 14:23:06 +0900

    Add : community-feed-ranking

---
+++ src/components/Community/FeedRanking.tsx
@@ -0,0 +1,11 @@
+import { RankingArea, RankingSubtitle } from './Feed.style'
+
+const FeedRanking = (props) => {
+    return (
+        <RankingArea>
+            <RankingSubtitle>인기 게시글</RankingSubtitle>
+        </RankingArea>
+    )
+}
+
+export default FeedRanking
