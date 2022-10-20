3a45b9c2bbcaeb1dc27baa424628c92cfe2919c5
JunSeok <ppu1234@naver.com>
2022-10-20 22:25:30 +0900

    Added : community-feed-material-ui-pagination

--- src/components/Community/FeedNavigation.tsx
+++ src/components/Community/FeedNavigation.tsx
@@ -1,25 +1,30 @@
 import { FeedTableContainer } from './Feed.style'
 import Link from 'next/link'
+import { Pagination } from '@material-ui/lab'
 
 const FeedNavigation = (props) => {
     const { totalPage } = props
 
     return (
-        <FeedTableContainer>
-            <tbody>
-                <tr>
-                    <td>
-                        <Link href={'/community/feed/1'}>1</Link>
-                    </td>
-                    <td>
-                        <Link href={'/community/feed/2'}>2</Link>
-                    </td>
-                    <td>
-                        <Link href={'/community/feed/3'}>3</Link>
-                    </td>
-                </tr>
-            </tbody>
-        </FeedTableContainer>
+        <>
+            <Pagination count={totalPage} variant='outlined' color='primary' />
+        </>
+
+        // <FeedTableContainer>
+        //     <tbody>
+        //         <tr>
+        //             <td>
+        //                 <Link href={'/community/feed/1'}>1</Link>
+        //             </td>
+        //             <td>
+        //                 <Link href={'/community/feed/2'}>2</Link>
+        //             </td>
+        //             <td>
+        //                 <Link href={'/community/feed/3'}>3</Link>
+        //             </td>
+        //         </tr>
+        //     </tbody>
+        // </FeedTableContainer>
     )
 }
 
