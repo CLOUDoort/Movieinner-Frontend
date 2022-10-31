280ad42b4e890d3989f957e79f8d801b5668bde9
JunSeok <ppu1234@naver.com>
2022-10-31 13:46:13 +0900

    Updated : community-post-modify

--- src/components/Community/ModifyPost.tsx
+++ src/components/Community/ModifyPost.tsx
@@ -1,10 +1,12 @@
 import { useRouter } from 'next/router'
+import useGetPostData from '../react-query/PostData'
 import WysiwygEditor from './WysiwygEditor'
 
 const ModifyPost = () => {
     const router = useRouter()
     const { idx } = router.query
-
+    const { data, isLoading } = useGetPostData(idx ? idx : null)
+    console.log('postData', data)
     return (
         <>
             <WysiwygEditor />
