9d755e6c7d7246572f63049e3df702b7effa8276
JunSeok <ppu1234@naver.com>
2022-11-09 16:58:44 +0900

    Fixed : etc

--- src/components/Community/Post/Comment/PostComment.tsx
+++ src/components/Community/Post/Comment/PostComment.tsx
@@ -2,7 +2,7 @@ import { useSelector } from 'react-redux'
 import { RootState } from '../../../../store/store'
 import PostRootWrite from './PostRootWrite'
 import { CommentArea } from './PostComment.style'
-import PostCommentList from './PostCommentLists'
+import PostCommentLists from './PostCommentLists'
 
 const PostComment = (props) => {
     const { idx, commentList, refreshFunction } = props
@@ -11,7 +11,7 @@ const PostComment = (props) => {
 
     return (
         <CommentArea>
-            <PostCommentList nickname={nickname} refreshFunction={refreshFunction} commentList={commentList} accessToken={accessToken} />
+            <PostCommentLists nickname={nickname} refreshFunction={refreshFunction} commentList={commentList} accessToken={accessToken} />
             <PostRootWrite refreshFunction={refreshFunction} accessToken={accessToken} idx={idx} nickname={nickname} />
         </CommentArea>
     )
