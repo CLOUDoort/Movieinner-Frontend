437cf41e83165f677c4a9e5497f9898ee0044ed9
JunSeok <ppu1234@naver.com>
2022-11-11 12:07:43 +0900

    Fixed : etc

--- src/components/Community/Post/Post.tsx
+++ src/components/Community/Post/Post.tsx
@@ -37,7 +37,7 @@ const Post = () => {
     const refreshFunction = (newComment) => {
         setCommentList(commentList.concat(newComment))
     }
-    console.log('commentList', commentList)
+
     return (
         <>
             {idx && !isLoading ? (
