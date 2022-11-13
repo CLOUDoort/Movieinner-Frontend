b834cd1de5feba51723fe4726a5217aea799736d
JunSeok <ppu1234@naver.com>
2022-11-13 19:53:38 +0900

    Refactoring : community-feed-hit-react-query

---
+++ src/components/react-query/HitFeedData.tsx
@@ -0,0 +1,23 @@
+import { apiInstance } from '../../apis/setting'
+import { AxiosResponse, AxiosError } from 'axios'
+import { dehydrate, QueryClient, useQuery } from 'react-query'
+import { GetServerSideProps } from 'next'
+
+export const getServerSidePropsHitFeed: GetServerSideProps = async (context) => {
+    const queryClient = new QueryClient()
+    await queryClient.prefetchQuery(['hitFeed'], () => useGetHitFeed())
+    return {
+        props: {
+            dehydratedState: dehydrate(queryClient),
+        },
+    }
+}
+
+export const getHitFeed = () => apiInstance.get(`/community/content/hit`, { withCredentials: true })
+
+const useGetHitFeed = () => {
+    const queryFn = () => getHitFeed()
+    return useQuery<AxiosResponse<any>, AxiosError>(['hitFeed'], queryFn)
+}
+
+export default useGetHitFeed
