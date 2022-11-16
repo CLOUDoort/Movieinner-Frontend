7a65f5d3caf4606066e98ee55852fb73eb8c1371
JunSeok <ppu1234@naver.com>
2022-11-16 14:14:17 +0900

    Updated : Loading_style

--- src/components/Loading.style.tsx
+++ src/components/Loading.style.tsx
@@ -3,9 +3,9 @@ import styled from '@emotion/styled'
 export const LoadingPage = styled.div`
     display: flex;
     width: 100%;
-    height: 100%;
+    min-height: 100vh;
     justify-content: center;
-    /* align-items: center; */
+    align-items: center;
 `
 
 export const LoadingContainer = styled.div`
