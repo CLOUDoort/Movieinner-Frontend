2c288b5acac9826b3a8d7657a1bbdeb00754b916
JunSeok <ppu1234@naver.com>
2022-11-27 21:29:24 +0900

    Added : Category_style

---
+++ src/components/Category.style.tsx
@@ -0,0 +1,29 @@
+import styled from '@emotion/styled'
+
+export const CategoryContainer = styled.div`
+    position: fixed;
+    top: 0;
+    left: 0;
+    right: 0;
+    bottom: 0;
+    z-index: 1000;
+`
+
+export const CategoryBox = styled.div`
+    position: absolute;
+    top: 80px;
+    left: 450px;
+    right: 0;
+    bottom: 0;
+    background-color: #ff0000;
+    width: 300px;
+    height: 200px;
+    border-radius: 5px;
+`
+
+export const CategoryList = styled.div`
+    display: grid;
+    grid-template-columns: 1fr 1fr 1fr;
+    margin-left: 20px;
+    margin-top: 15px;
+`
