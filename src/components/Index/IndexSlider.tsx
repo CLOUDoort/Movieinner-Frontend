de5bf172e5f6ad1f8b36be20ffd31ccf86ae5b57
JunSeok <ppu1234@naver.com>
2022-11-30 15:28:41 +0900

    Added : index_theme_slider

--- src/components/Index/IndexSlider.tsx
+++ src/components/Index/IndexSlider.tsx
@@ -4,62 +4,42 @@ import { Swiper, SwiperSlide } from 'swiper/react'
 
 // import required modules
 import { Autoplay, Pagination, Navigation } from 'swiper'
-import { useEffect, useState } from 'react'
-import { apiInstance } from '../../apis/setting'
 
-const IndexSlider = () => {
-    const [image, setImage] = useState({})
 
-    // 6개 테마의 백드롭 이미지 불러오기
-    useEffect(() => {
-        const getImage = async () => {
-            const backDropImage = await apiInstance.get('')
-        }
-        getImage()
-    })
+const IndexSlider = (props) => {
+    const { sliderImage } = props
     return (
-        <>
-            <Swiper
-                cssMode={true}
-                autoplay={{
-                    delay: 3000,
-                    disableOnInteraction: false,
-                }}
-                slidesPerView={1}
-                spaceBetween={30}
-                loop={true}
-                pagination={{
-                    clickable: true,
-                }}
-                navigation={true}
-                modules={[Autoplay, Pagination, Navigation]}
-                className='mySwiper'
-            >
-                <SwiperSlide>
+
+        <Swiper
+            cssMode={true}
+            autoplay={{
+                delay: 3000,
+                disableOnInteraction: false,
+            }}
+            slidesPerView={1}
+            spaceBetween={30}
+            loop={true}
+            pagination={{
+                clickable: true,
+            }}
+            navigation={true}
+            modules={[Autoplay, Pagination, Navigation]}
+            className='mySwiper'
+        >
+            {sliderImage.map((obj) => (
+                <SwiperSlide key={obj.movie_id}>
                     <Image
-                        src='https://image.tmdb.org/t/p/w500/odJ4hx6g6vBt4lBWKFD1tI8WS4x.jpg'
-                        alt='이미지'
-                        width={700}
-                        height={300}
+                        src={`https://image.tmdb.org/t/p/w1280${obj.backdrop_path}`}
+                        id={obj.movie_name}
+                        // onClick={openModal}
+                        layout='fill'
+                        priority={true}
                     />
+                    <div>{obj.theme_name}</div>
                 </SwiperSlide>
-                <SwiperSlide>
-                    <Image src='https://image.tmdb.org/t/p/w500/odJ4hx6g6vBt4lBWKFD1tI8WS4x.jpg' alt='이미지' width={700} height={300} />
-                </SwiperSlide>
-                <SwiperSlide>
-                    <Image src='https://image.tmdb.org/t/p/w500/odJ4hx6g6vBt4lBWKFD1tI8WS4x.jpg' alt='이미지' width={700} height={300} />
-                </SwiperSlide>
-                <SwiperSlide>
-                    <Image src='https://image.tmdb.org/t/p/w500/odJ4hx6g6vBt4lBWKFD1tI8WS4x.jpg' alt='이미지' width={700} height={300} />
-                </SwiperSlide>
-                <SwiperSlide>
-                    <Image src='https://image.tmdb.org/t/p/w500/odJ4hx6g6vBt4lBWKFD1tI8WS4x.jpg' alt='이미지' width={700} height={300} />
-                </SwiperSlide>
-                <SwiperSlide>
-                    <Image src='https://image.tmdb.org/t/p/w500/odJ4hx6g6vBt4lBWKFD1tI8WS4x.jpg' alt='이미지' width={700} height={300} />
-                </SwiperSlide>
-            </Swiper>
-        </>
+            ))}
+        </Swiper>
+
     )
 }
 
