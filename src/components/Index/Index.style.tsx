9d4af7af0b128c34f558a3ee285c6b3181af1790
JunSeok <ppu1234@naver.com>
2022-11-30 15:27:55 +0900

    Updated : index_style

--- src/components/Index/Index.style.tsx
+++ src/components/Index/Index.style.tsx
@@ -3,4 +3,12 @@ import styled from '@emotion/styled'
 export const IndexBox = styled.div`
     margin-top: 30px;
     width: 100%;
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+    > div {
+        margin-top: 20px;
+        margin-bottom: 20px;
+    }
 `
