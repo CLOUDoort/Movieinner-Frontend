fb13b43adbf90669bf3e12f2e6b05344bf8ac8cb
JunSeok <ppu1234@naver.com>
2022-11-30 15:28:26 +0900

    Added : index_theme_slider

--- src/components/Index/Index.tsx
+++ src/components/Index/Index.tsx
@@ -1,23 +1,38 @@
-import React from 'react'
-import { useSelector } from 'react-redux'
-import { RootState } from '../../store/store'
+import React, { useEffect, useState } from 'react'
 import useGetPopularMovie from '../react-query/PopularMovie'
+import useGetThemeMovie from '../react-query/ThemeMovie'
 import { IndexBox } from './Index.style'
 import IndexSlider from './IndexSlider'
+import IndexPopularList from './IndexPopularList'
 
 const Index = () => {
-    const loginToken = useSelector((state: RootState) => state.token.token)
-    // // const [verifyEmail, setVerifyEmail] = useState(false)
-    // const verifyEmail = await apiInstance.get('/verify')
-    // if (verifyEmail) toast.success('메일 인증에 성공하셨습니다!')
     const popularMovie = useGetPopularMovie().data
-    console.log('popular', popularMovie?.data)
+    const themeMovie = useGetThemeMovie().data
+    const isLoading = useGetThemeMovie().isLoading
+    const [sliderImage, setSliderImage] = useState([])
+    console.log('po', popularMovie?.data)
+    useEffect(() => {
+        const getResponse = () => {
+            if (!isLoading) {
+                const sliderImageBox = Object.entries(themeMovie?.data).map(([key, value]) => ({
+                    movie_id: value[0].movie_id,
+                    movie_name: value[0].movie_name,
+                    backdrop_path: value[0].backdrop_path,
+                    theme_name: value[0].theme_name
+                }))
+                setSliderImage(sliderImageBox)
+            }
+        }
+        getResponse()
+    }, [isLoading])
 
     return (
-        <IndexBox>
-            <IndexSlider />
-        </IndexBox>
-
+        <>
+            {!isLoading && <IndexBox>
+                <IndexSlider sliderImage={sliderImage} />
+                <IndexPopularList popularMovie={popularMovie?.data} />
+            </IndexBox>}
+        </>
     )
 }
 
