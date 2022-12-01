41fdd5076767e3e56033b89651800090e118774e
JunSeok <ppu1234@naver.com>
2022-12-01 23:50:39 +0900

    Updated : index_popularList

--- src/components/Index/IndexPopularList.tsx
+++ src/components/Index/IndexPopularList.tsx
@@ -1,8 +1,10 @@
 import { ActorAreaCard, ActorImageBox, ActorTitleBox, MovieActorContainer, MovieActorList, MovieActorScroll } from "../MovieInfo/MovieInfo.style"
 import Image from "next/image"
+import { useRouter } from "next/router"
 
 const IndexPopularList = (props) => {
     const { popularMovie } = props
+    const router = useRouter()
     return (
         <MovieActorScroll>
             <div>금주의 인기 영화</div>
@@ -12,7 +14,7 @@ const IndexPopularList = (props) => {
                         {popularMovie?.map((obj: any) => (
                             <ActorAreaCard key={obj.id}>
                                 <div>
-                                    <ActorImageBox>
+                                    <ActorImageBox onClick={() => router.push(`/movie/${obj.id}`)}>
                                         <Image
                                             src={`https://image.tmdb.org/t/p/original/${obj.poster_path}`}
                                             id={obj.movie_name}
@@ -22,7 +24,7 @@ const IndexPopularList = (props) => {
                                         />
                                     </ActorImageBox>
                                     <ActorTitleBox>
-                                        <strong>{obj.title}</strong>
+                                        <strong onClick={() => router.push(`/movie/${obj.id}`)}>{obj.title}</strong>
                                         <p>{obj.release_date}</p>
                                     </ActorTitleBox>
                                 </div>
