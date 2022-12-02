a0ff3ffbb68438c9223aa7aa5df6029258d3769f
JunSeok <ppu1234@naver.com>
2022-12-03 00:31:39 +0900

    Updated : etc

--- src/components/MovieInfo/MovieActorInfo.tsx
+++ src/components/MovieInfo/MovieActorInfo.tsx
@@ -4,30 +4,28 @@ import { ActorAreaCard, ActorImageBox, ActorTitleBox, MovieActorContainer, Movie
 const MovieActorInfo = (props) => {
     const { actorInfo } = props
     return (
-        <>
-            <MovieActorScroll>
-                <div>감독&#47;출연</div>
-                <MovieActorContainer>
-                    <div>
-                        <MovieActorList>
-                            {actorInfo.map((obj: any) => (
-                                <ActorAreaCard key={obj.name}>
-                                    <div>
-                                        <ActorImageBox>
-                                            <Image src={`https://image.tmdb.org/t/p/original${obj.profile_path}`} layout='fill' alt={obj.name} />
-                                        </ActorImageBox>
-                                        <ActorTitleBox>
-                                            <strong>{obj.name}</strong>
-                                            <p>{obj.character}</p>
-                                        </ActorTitleBox>
-                                    </div>
-                                </ActorAreaCard>
-                            ))}
-                        </MovieActorList>
-                    </div>
-                </MovieActorContainer>
-            </MovieActorScroll>
-        </>
+        <MovieActorScroll>
+            <div>감독&#47;출연</div>
+            <MovieActorContainer>
+                <div>
+                    <MovieActorList>
+                        {actorInfo.map((obj: any) => (
+                            <ActorAreaCard key={obj.name}>
+                                <div>
+                                    <ActorImageBox>
+                                        <Image src={`https://image.tmdb.org/t/p/original${obj.profile_path}`} layout='fill' alt={obj.name} />
+                                    </ActorImageBox>
+                                    <ActorTitleBox>
+                                        <strong>{obj.name}</strong>
+                                        <p>{obj.character}</p>
+                                    </ActorTitleBox>
+                                </div>
+                            </ActorAreaCard>
+                        ))}
+                    </MovieActorList>
+                </div>
+            </MovieActorContainer>
+        </MovieActorScroll>
     )
 }
 
