90fcafd3284667d29f40fa234923d2be943fb7c6
JunSeok <ppu1234@naver.com>
2022-12-03 02:25:23 +0900

    Added : user_style

---
+++ src/components/User/User.style.tsx
@@ -0,0 +1,34 @@
+import styled from '@emotion/styled'
+
+export const UserBox = styled.div`
+    width: 100%;
+    height: 100vh;
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+    color: white;
+`
+
+export const UserProfileBox = styled.div`
+    min-width: 800px;
+`
+export const UserProfileContainer = styled.div`
+    min-width: 800px;
+    border: 1px solid gray;
+    display: flex;
+    justify-content: space-evenly;
+    align-items: center;
+`
+export const UserProfileInfo = styled.div`
+    display: flex;
+`
+export const UserProfileData = styled.div`
+    
+`
+
+export const UserDataBox = styled.div`
+    min-width: 800px;
+    display: grid;
+    grid-template-columns: 1fr 1fr;
+`
