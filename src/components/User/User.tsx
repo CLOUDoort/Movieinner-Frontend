00f9ad3f89a542144238ec5ce92c52807f4fecd1
JunSeok <ppu1234@naver.com>
2022-12-04 14:18:27 +0900

    Updated : user

--- src/components/User/User.tsx
+++ src/components/User/User.tsx
@@ -1,6 +1,4 @@
 import { useRouter } from 'next/router'
-import { useEffect } from 'react'
-import { apiInstance } from '../../apis/setting'
 import { UserBox } from './User.style'
 import UserData from './UserData/UserData'
 import UserProfile from './UserProfile/UserProfile'
@@ -9,25 +7,6 @@ const User = () => {
     const router = useRouter()
     const { nickname } = router.query
 
-    useEffect(() => {
-        const getResponse = async () => {
-            try {
-                const themeResponse = await apiInstance.get('/movies/liked/theme', { params: { nickname: nickname } })
-                console.info(themeResponse.data.liked)
-                const movieResponse = await apiInstance.get('/movies/liked/movie', {
-                    params: {
-                        nickname: nickname,
-                    },
-                })
-                console.info(movieResponse.data.liked)
-            } catch (e) {
-                console.error(e.response)
-            }
-        }
-        getResponse()
-    }, [])
-
-    console.log(nickname)
     return (
         <UserBox>
             <UserProfile nickname={nickname} />
