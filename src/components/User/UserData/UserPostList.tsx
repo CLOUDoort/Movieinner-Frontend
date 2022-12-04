a556ccd1a8cd659237bccd19b78111854816f8e4
JunSeok <ppu1234@naver.com>
2022-12-04 14:19:20 +0900

    Added : user_data_postList

--- src/components/User/UserData/UserPostList.tsx
+++ src/components/User/UserData/UserPostList.tsx
@@ -1,5 +1,12 @@
+import { UserDataContainer, UserDataList } from "./UserData.style"
+
 const UserPostList = (props) => {
-    return <></>
+    return (
+        <UserDataContainer>
+            <div>작성한 게시물</div>
+            <UserDataList></UserDataList>
+        </UserDataContainer>
+    )
 }
 
 export default UserPostList
