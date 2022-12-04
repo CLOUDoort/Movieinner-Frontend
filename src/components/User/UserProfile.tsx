d16bfeb84bee11f0fe405ced39a67f31003066c3
JunSeok <ppu1234@naver.com>
2022-12-04 13:37:01 +0900

    Updated : etc

--- src/components/User/UserProfile.tsx
+++ src/components/User/UserProfile.tsx
@@ -5,21 +5,19 @@ import { BsFillPencilFill } from "react-icons/bs"
 const UserProfile = (props) => {
     const { nickname } = props
     return (
-        <>
-            <UserProfileBox>
-                <UserProfileContainer>
-                    <UserProfileInfo>
-                        <Image src={`/blank.png`} width={60} height={60} />
-                        <span>{nickname}</span>
-                        <span><BsFillPencilFill size={20} /></span>
-                    </UserProfileInfo>
-                    <UserProfileData>
-                        <div>개인정보 수정</div>
-                        <div>로그아웃</div>
-                    </UserProfileData>
-                </UserProfileContainer>
-            </UserProfileBox>
-        </>
+        <UserProfileBox>
+            <UserProfileContainer>
+                <UserProfileInfo>
+                    <Image src={`/blank.png`} width={60} height={60} />
+                    <span>{nickname}</span>
+                    <span><BsFillPencilFill size={20} /></span>
+                </UserProfileInfo>
+                <UserProfileData>
+                    <div>개인정보 수정</div>
+                    <div>로그아웃</div>
+                </UserProfileData>
+            </UserProfileContainer>
+        </UserProfileBox>
     )
 }
 
