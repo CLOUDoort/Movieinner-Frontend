4ad240bd96108899009cb316a5ccc21b5d9e7b94
JunSeok <ppu1234@naver.com>
2022-12-04 14:18:20 +0900

    Updated : user_style

--- src/components/User/User.style.tsx
+++ src/components/User/User.style.tsx
@@ -5,50 +5,6 @@ export const UserBox = styled.div`
     height: 100vh;
     display: flex;
     flex-direction: column;
-    /* justify-content: center; */
     align-items: center;
     color: white;
-`
-
-export const UserProfileBox = styled.div`
-    margin-top: 20px;
-    min-width: 800px;
-    `
-export const UserProfileContainer = styled.div`
-    border-radius: 10px;
-    padding: 30px 24px;
-    min-width: 800px;
-    border: 1px solid gray;
-    display: flex;
-    justify-content: space-between;
-    align-items: center;
-`
-export const UserProfileInfo = styled.div`
-    display: flex;
-    align-items: center;
-    > :first-of-type {
-        border-radius: 50%;
-    }
-    > :nth-of-type(2) {
-        margin-left: 20px;
-        font-weight: bold;
-    }
-    > :nth-of-type(3) {
-        margin-left: 10px;
-        margin-top: 5px;
-        transition: color 0.5s;
-        :hover {
-            color: red;
-        }
-    }
-`
-export const UserProfileData = styled.div`
-    display: flex;
-    flex-direction: column;
-`
-
-export const UserDataBox = styled.div`
-    min-width: 800px;
-    display: grid;
-    grid-template-columns: 1fr 1fr;
 `
