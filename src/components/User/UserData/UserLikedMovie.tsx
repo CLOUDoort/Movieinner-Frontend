d74cf87fc5caf0edce7479f888fe5dc80f43ee0d
JunSeok <ppu1234@naver.com>
2022-12-05 21:14:33 +0900

    Added : user_liked_movie

---
+++ src/components/User/UserData/UserLikedMovie.tsx
@@ -0,0 +1,33 @@
+import Image from 'next/image'
+import Router from 'next/router'
+import { StorageMovieScroll, StorageMovieContainer, StorageMovieList, StorageMovieAreaCard, StorageMovieImgBox, StorageMovieTitleBox } from './Storage/Storage.style'
+
+const UserLikedMovie = (props) => {
+    const { userLikedMovie } = props
+
+    return (
+        <StorageMovieScroll>
+            <p>좋아요 누른 영화</p>
+            <StorageMovieContainer>
+                <div>
+                    <StorageMovieList>
+                        {userLikedMovie.map((obj) => (
+                            <StorageMovieAreaCard key={obj.idx}>
+                                <div>
+                                    <StorageMovieImgBox >
+                                        <Image src={`https://image.tmdb.org/t/p/original${obj.poster_path}`} layout='fill' alt={obj.name} />
+                                    </StorageMovieImgBox>
+                                    <StorageMovieTitleBox>
+                                        <strong >{obj.name}</strong>
+                                    </StorageMovieTitleBox>
+                                </div>
+                            </StorageMovieAreaCard>
+                        ))}
+                    </StorageMovieList>
+                </div>
+            </StorageMovieContainer>
+        </StorageMovieScroll>
+    )
+}
+
+export default UserLikedMovie
