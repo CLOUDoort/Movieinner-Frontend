787fcce899f5354615bcbf91e50f29167ae06715
JunSeok <ppu1234@naver.com>
2022-12-05 21:14:44 +0900

    Added : user_liked_theme

---
+++ src/components/User/UserData/UserLikedTheme.tsx
@@ -0,0 +1,33 @@
+import Image from 'next/image'
+import Router from 'next/router'
+import { StorageMovieAreaCard, StorageMovieContainer, StorageMovieImgBox, StorageMovieList, StorageMovieScroll, StorageMovieTitleBox } from './Storage/Storage.style'
+
+
+const UserLikedTheme = (props) => {
+    const { userLikedTheme } = props
+    return (
+        <StorageMovieScroll>
+            <p>찜한 테마</p>
+            <StorageMovieContainer>
+                <div>
+                    <StorageMovieList>
+                        {userLikedTheme.map((obj) => (
+                            <StorageMovieAreaCard key={obj.idx}>
+                                <div>
+                                    <StorageMovieImgBox >
+                                        <Image src={`https://image.tmdb.org/t/p/original${obj.backdrop_path}`} layout='fill' alt={obj.name} />
+                                    </StorageMovieImgBox>
+                                    <StorageMovieTitleBox>
+                                        <strong >{obj.name}</strong>
+                                    </StorageMovieTitleBox>
+                                </div>
+                            </StorageMovieAreaCard>
+                        ))}
+                    </StorageMovieList>
+                </div>
+            </StorageMovieContainer>
+        </StorageMovieScroll>
+    )
+}
+
+export default UserLikedTheme
