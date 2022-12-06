2dc6be0f522fb44b647953f14f2995782b1458f8
JunSeok <ppu1234@naver.com>
2022-12-06 23:43:37 +0900

    Updated : user_data_loading & number

--- src/components/User/User.tsx
+++ src/components/User/User.tsx
@@ -1,4 +1,5 @@
 import { useRouter } from 'next/router'
+import Loading from '../Loading'
 import useGetUserLiked from '../react-query/UserLiked'
 import useGetUserPosts from '../react-query/UserPosts'
 import { UserBox } from './User.style'
@@ -11,16 +12,22 @@ const User = () => {
     const userPosts = useGetUserPosts(nickname).data
     const userLikedMovie = useGetUserLiked('movie', nickname).data
     const userLikedTheme = useGetUserLiked('theme', nickname).data
+    const movieLoading = useGetUserLiked('movie', nickname).isLoading
+    const themeLoading = useGetUserLiked('theme', nickname).isLoading
+    const postsLoading = useGetUserLiked('movie', nickname).isLoading
 
-
-    console.log('movie', userLikedMovie?.data?.liked)
-    console.log('theme', userLikedTheme?.data?.liked)
+    let number = 1;
+    const userPostsList = userPosts?.data?.response.map((obj) => ({
+        ...obj, number: number++
+    }))
 
     return (
-        <UserBox>
-            <UserProfile nickname={nickname} />
-            <UserData nickname={nickname} userPostList={userPosts?.data?.response} userLikedMovie={userLikedMovie?.data?.liked} userLikedTheme={userLikedTheme?.data?.liked} />
-        </UserBox>
+        <>
+            {!movieLoading && !themeLoading && !postsLoading ? <UserBox>
+                <UserProfile nickname={nickname} />
+                <UserData nickname={nickname} userPostList={userPostsList} userLikedMovie={userLikedMovie?.data?.liked} userLikedTheme={userLikedTheme?.data?.liked} />
+            </UserBox> : <Loading />}
+        </>
     )
 }
 
