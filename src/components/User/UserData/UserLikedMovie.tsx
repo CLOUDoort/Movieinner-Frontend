4fac3d5165081aeb35988a3e27bd5ff09310bbfd
JunSeok <ppu1234@naver.com>
2022-12-06 23:42:59 +0900

    Updated : user_liked_movie

--- src/components/User/UserData/UserLikedMovie.tsx
+++ src/components/User/UserData/UserLikedMovie.tsx
@@ -1,10 +1,10 @@
 import Image from 'next/image'
-import Router from 'next/router'
+import { useRouter } from 'next/router'
 import { StorageMovieScroll, StorageMovieContainer, StorageMovieList, StorageMovieAreaCard, StorageMovieImgBox, StorageMovieTitleBox } from './UserStorage.style'
 
 const UserLikedMovie = (props) => {
     const { userLikedMovie } = props
-
+    const router = useRouter()
     return (
         <StorageMovieScroll>
             <p>좋아요 누른 영화</p>
@@ -14,11 +14,11 @@ const UserLikedMovie = (props) => {
                         {userLikedMovie.map((obj) => (
                             <StorageMovieAreaCard key={obj.idx}>
                                 <div>
-                                    <StorageMovieImgBox >
+                                    <StorageMovieImgBox onClick={() => router.push(`/movie/${obj.movie_id}`)}>
                                         <Image src={`https://image.tmdb.org/t/p/original${obj.poster_path}`} layout='fill' alt={obj.name} />
                                     </StorageMovieImgBox>
                                     <StorageMovieTitleBox>
-                                        <strong >{obj.name}</strong>
+                                        <strong onClick={() => router.push(`/movie/${obj.movie_id}`)}>{obj.name}</strong>
                                     </StorageMovieTitleBox>
                                 </div>
                             </StorageMovieAreaCard>
