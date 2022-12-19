9b79995986d271baf0c61c035d1b73dd8638aeb2
JunSeok <ppu1234@naver.com>
2022-12-19 17:23:02 +0900

    Updated : currentState_style

--- src/components/Auth/CurrentStatus/CurrentStatus.style.tsx
+++ src/components/Auth/CurrentStatus/CurrentStatus.style.tsx
@@ -3,22 +3,29 @@ import styled from '@emotion/styled'
 export const CurrentStatusDiv = styled.div`
     display: flex;
     flex-direction: column;
+    justify-content: center;
+    align-items: center;
     margin-top: 80px;
     position: relative;
 `
 export const CurrentTextDiv = styled.div`
     width: 600px;
     display: flex;
-    flex-direction: row;
     justify-content: space-between;
     font-size: 15px;
     position: absolute;
     z-index: 100;
     bottom: 120px;
+    > :nth-of-type(1) {
+        padding-left: 10px;
+    }
+    > :nth-of-type(2) {
+        padding-left: 15px;
+    }
 `
 export const HorizontalRule = styled.div`
     > hr {
-        width: 600px;
+        width: 550px;
         margin-bottom: 75px;
         position: relative;
     }
@@ -28,7 +35,7 @@ export const CircleBox = styled.div`
     display: flex;
     flex-direction: row;
     justify-content: space-between;
-    width: 600px;
+    width: 550px;
     position: absolute;
     bottom: 60px;
 `
