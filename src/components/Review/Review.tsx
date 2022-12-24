bf577a1eb97cb0218277a1aef048275e81763ae3
JunSeok <ppu1234@naver.com>
2022-12-24 19:16:12 +0900

    Deleted

--- src/components/Review/Review.tsx
+++
@@ -1,5 +0,0 @@
-const Review = () => {
-    return <>Review</>
-}
-
-export default Review
--- src/pages/review/index.tsx
+++
@@ -1,7 +0,0 @@
-import Review from "../../components/Review/Review"
-
-const review = () => {
-    return <Review />
-}
-
-export default review
