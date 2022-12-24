e644a7799e6e06fb1232c3022b1771d414215b30
JunSeok <ppu1234@naver.com>
2022-12-24 18:39:58 +0900

    Updated : etc

--- src/components/Auth/Signup/SignupVerify.tsx
+++ src/components/Auth/Signup/SignupVerify.tsx
@@ -40,31 +40,29 @@ const SignupVerify = () => {
         }
     }
     return (
-        <>
-            <SignupVerifyContainer>
-                <ContainerTitle>인증 이메일 전송 완료</ContainerTitle>
-                <ContainerProfile>
-                    <Image src={image} width={150} height={150} alt='프로필 이미지입니다.' />
-                    <div>{userData.nickname}님 환영합니다 &#33;</div>
-                </ContainerProfile>
-                <ContainerText>
-                    <div>인증 메일이 {userData.email}&#40;으&#41;로 전송되었습니다.</div>
-                    <div>받으신 이메일을 열어 링크로 접속하시면 가입이 완료됩니다.</div>
-                </ContainerText>
-                <ContainerBtn>
-                    <div>
-                        <Link href='/'>
-                            <button>Home</button>
-                        </Link>
-                        <button onClick={handleClick}>인증 메일 다시 보내기</button>
-                    </div>
-                    <div>
-                        {email.click === true && email.send === true && <CheckText check={true}>다시 보내기 성공!</CheckText>}
-                        {email.click === true && <CheckText check={false}>이메일을 보내지 못했습니다.</CheckText>}
-                    </div>
-                </ContainerBtn>
-            </SignupVerifyContainer>
-        </>
+        <SignupVerifyContainer>
+            <ContainerTitle>인증 이메일 전송 완료</ContainerTitle>
+            <ContainerProfile>
+                <Image src={image} width={150} height={150} alt='프로필 이미지입니다.' />
+                <div>{userData.nickname}님 환영합니다 &#33;</div>
+            </ContainerProfile>
+            <ContainerText>
+                <div>인증 메일이 {userData.email}&#40;으&#41;로 전송되었습니다.</div>
+                <div>받으신 이메일을 열어 링크로 접속하시면 가입이 완료됩니다.</div>
+            </ContainerText>
+            <ContainerBtn>
+                <div>
+                    <Link href='/'>
+                        <button>Home</button>
+                    </Link>
+                    <button onClick={handleClick}>인증 메일 다시 보내기</button>
+                </div>
+                <div>
+                    {email.click === true && email.send === true && <CheckText check={true}>다시 보내기 성공!</CheckText>}
+                    {email.click === true && <CheckText check={false}>이메일을 보내지 못했습니다.</CheckText>}
+                </div>
+            </ContainerBtn>
+        </SignupVerifyContainer>
     )
 }
 
--- src/components/Auth/Signup/SignupVerifyFail.tsx
+++ src/components/Auth/Signup/SignupVerifyFail.tsx
@@ -2,7 +2,7 @@ import Link from 'next/link'
 import { useState } from 'react'
 import { apiInstance } from '../../../apis/setting'
 import { CheckText } from './Signup.style'
-import { Container, ContainerBtn, ContainerTitle, VerifyFailText } from './SignupVerify.style'
+import { SignupVerifyContainer, ContainerBtn, ContainerTitle, VerifyFailText } from './SignupVerify.style'
 
 const SignupVerifyFail = (props) => {
     const [sendEmail, setSendEmail] = useState({
@@ -28,26 +28,24 @@ const SignupVerifyFail = (props) => {
         }
     }
     return (
-        <>
-            <Container>
-                <ContainerTitle>이메일 인증 실패</ContainerTitle>
-                <VerifyFailText>
-                    <p>인증에 실패했습니다!</p>
-                </VerifyFailText>
-                <ContainerBtn>
-                    <div>
-                        <Link href='/'>
-                            <button>Home</button>
-                        </Link>
-                        <button onClick={handleClick}>인증 메일 다시 보내기</button>
-                    </div>
-                    <div>
-                        {sendEmail.click === true && sendEmail.send === true && <CheckText check={true}>다시 보내기 성공!</CheckText>}
-                        {sendEmail.click === true && sendEmail.send === false && <CheckText check={false}>에러 발생!</CheckText>}
-                    </div>
-                </ContainerBtn>
-            </Container>
-        </>
+        <SignupVerifyContainer>
+            <ContainerTitle>이메일 인증 실패</ContainerTitle>
+            <VerifyFailText>
+                <p>인증에 실패했습니다!</p>
+            </VerifyFailText>
+            <ContainerBtn>
+                <div>
+                    <Link href='/'>
+                        <button>Home</button>
+                    </Link>
+                    <button onClick={handleClick}>인증 메일 다시 보내기</button>
+                </div>
+                <div>
+                    {sendEmail.click === true && sendEmail.send === true && <CheckText check={true}>다시 보내기 성공!</CheckText>}
+                    {sendEmail.click === true && sendEmail.send === false && <CheckText check={false}>에러 발생!</CheckText>}
+                </div>
+            </ContainerBtn>
+        </SignupVerifyContainer>
     )
 }
 
