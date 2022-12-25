e504c7b937eb2b0e43c826d35065500057063106
JunSeok <ppu1234@naver.com>
2022-12-25 19:33:08 +0900

    Updated : setting_style

---
+++ src/components/Setting.style.tsx
@@ -0,0 +1,22 @@
+import styled from '@emotion/styled'
+
+export const SettingContainer = styled.div`
+    width: 100%;
+    height: 100vh;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+`
+
+export const SettingBox = styled.div`
+    display: flex;
+    flex-direction: column;
+    min-width: 800px;
+    margin-top: 10px;
+`
+
+export const SettingDeleteUser = styled.div`
+    background-color: red;
+    padding: 8px 16px;
+    border-radius: 5px;
+`
