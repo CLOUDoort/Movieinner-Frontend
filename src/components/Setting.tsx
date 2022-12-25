e5089ada9de98bad4085d0a77a6df976e08cadbe
JunSeok <ppu1234@naver.com>
2022-12-25 19:33:20 +0900

    Added : setting_delete_user

--- src/components/Setting.tsx
+++ src/components/Setting.tsx
@@ -1,5 +1,25 @@
+import { useSelector } from "react-redux"
+import { apiInstance } from "../apis/setting"
+import { RootState } from "../store/store"
+import { SettingContainer, SettingDeleteUser } from "./Setting.style"
+
 const Setting = () => {
-    return <></>
+    const email = useSelector((state: RootState) => state.email.email)
+    const nickname = useSelector((state: RootState) => state.nickname.nickname)
+
+    const deleteUser = async () => {
+        const response = await apiInstance.delete(`/users/delete`, {
+            params: {
+                email: email, nickname: nickname
+            }
+        })
+        console.log('deleteResponse', response.data)
+    }
+    return (
+        <SettingContainer>
+            <SettingDeleteUser onClick={deleteUser}>회원탈퇴</SettingDeleteUser>
+        </SettingContainer>
+    )
 }
 
 export default Setting
