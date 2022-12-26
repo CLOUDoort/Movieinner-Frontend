46ed9c3ccc23de8a227af8d1bc386dec57a83375
JunSeok <ppu1234@naver.com>
2022-12-26 22:51:33 +0900

    Added : setting_delete_confirm

--- src/components/Setting.tsx
+++ src/components/Setting.tsx
@@ -1,4 +1,6 @@
+import { useRouter } from "next/router"
 import { useSelector } from "react-redux"
+import { toast } from "react-toastify"
 import { apiInstance } from "../apis/setting"
 import { RootState } from "../store/store"
 import { SettingContainer, SettingDeleteUser } from "./Setting.style"
@@ -6,14 +8,19 @@ import { SettingContainer, SettingDeleteUser } from "./Setting.style"
 const Setting = () => {
     const email = useSelector((state: RootState) => state.email.email)
     const nickname = useSelector((state: RootState) => state.nickname.nickname)
+    const router = useRouter()
 
     const deleteUser = async () => {
-        const response = await apiInstance.delete(`/users/delete`, {
-            params: {
-                email: email, nickname: nickname
-            }
-        })
-        console.log('deleteResponse', response.data)
+        if (confirm("정말 탈퇴하시겠습니까?")) {
+            const response = await apiInstance.delete(`/users/delete`, {
+                params: {
+                    email: email, nickname: nickname
+                }
+            })
+            toast.success('탈퇴 성공')
+            router.replace('/')
+            console.log('deleteResponse', response.data)
+        }
     }
     return (
         <SettingContainer>
