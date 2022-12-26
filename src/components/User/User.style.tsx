36fc4fe56b0c04fd8fe3e1c1841de46a55e41060
JunSeok <ppu1234@naver.com>
2022-12-26 22:51:19 +0900

    Updated : user_style

--- src/components/User/User.style.tsx
+++ src/components/User/User.style.tsx
@@ -2,6 +2,7 @@ import styled from '@emotion/styled'
 
 export const UserBox = styled.div`
     width: 100%;
+    min-width: 850px;
     height: 100%;
     display: flex;
     flex-direction: column;
