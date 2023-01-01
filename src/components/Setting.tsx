dcc26d1e3dde212768b85858a08cc5b3a1baf0db
JunSeok <ppu1234@naver.com>
2023-01-01 20:08:30 +0900

    Added : modify_nickname_error_message

--- src/components/Setting.tsx
+++ src/components/Setting.tsx
@@ -4,12 +4,18 @@ import { toast } from "react-toastify"
 import { apiInstance } from "../apis/setting"
 import { RootState } from "../store/store"
 import { SettingBox, SettingContainer, SettingDeleteUser } from "./Setting.style"
+import { UserModifyNickname, UserProfileInfo } from "./User/UserProfile/UserProfile.style"
+import Image from "next/image"
+import { useState } from "react"
+import { BsFillPencilFill } from "react-icons/bs"
 
 const Setting = () => {
     const email = useSelector((state: RootState) => state.email.email)
     const nickname = useSelector((state: RootState) => state.nickname.nickname)
-    const userIdx = useSelector((state: RootState) => state.idx.idx)
     const router = useRouter()
+    const userIdx = useSelector((state: RootState) => state.idx.idx)
+    const [modify, setModify] = useState(false)
+    const [newNickname, setnewNickname] = useState(nickname)
 
     const deleteUser = async () => {
         if (confirm("정말 탈퇴하시겠습니까?")) {
@@ -23,12 +29,42 @@ const Setting = () => {
             console.log('deleteResponse', response.data)
         }
     }
+    const clickModifyImg = () => {
+        setModify(!modify)
+        setnewNickname(nickname)
+    }
+    const handleChange = (e) => {
+        setnewNickname(e.target.value)
+    }
+    const clickModify = async () => {
+        try {
+            // 결과값으로 중복값 확인
+            const modifyNickname = await apiInstance.put(`/users/change/nickname`, { userIdx: userIdx, newNickname: newNickname })
+            if (!modifyNickname.data.success) toast.error('영어 대소문, 한글, 숫자, 특수기호(-,_,.) 포함 2~12글자만 허용됩니다.')
+            // 닉네임 변경 시 리프레시 토큰 재발급
+            await apiInstance.post(`/auth`, { email: email })
+            // dispatch(setNickname(newNickname))
+            if (modifyNickname.data.success) {
+                setModify(!modify)
+            }
+            router.reload()
+        } catch (e) {
+            console.error(e.response)
+        }
+    }
     return (
         <SettingContainer>
             <SettingBox>
                 <div>
-                    <div>이미지</div>
-                    <div>닉네임</div>
+                    <UserProfileInfo>
+                        <Image src={`/blank.png`} width={60} height={60} />
+                        {modify ?
+                            <UserModifyNickname><input type='text' value={newNickname} onChange={handleChange} />
+                                <button onClick={clickModify}>수정</button></UserModifyNickname>
+                            : <span>{nickname}</span>}
+                        {!modify ? <span onClick={clickModifyImg}><BsFillPencilFill size={20} /></span> : null}
+                        {modify ? <span onClick={clickModifyImg}>취소</span> : null}
+                    </UserProfileInfo>
                 </div>
                 <div>비밀번호 변경</div>
                 <SettingDeleteUser>
