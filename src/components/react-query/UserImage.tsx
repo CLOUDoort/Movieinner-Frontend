9d4556afed9996a251b29b65c96a6340f061d859
JunSeok <ppu1234@naver.com>
2023-01-05 23:58:58 +0900

    Added : user_image_upload

---
+++ src/components/react-query/UserImage.tsx
@@ -0,0 +1,26 @@
+import { AxiosError, AxiosResponse } from 'axios'
+import { GetServerSideProps } from 'next'
+import { dehydrate, QueryClient, useQuery } from 'react-query'
+import { apiInstance } from '../../apis/setting'
+export interface PostData {
+    userIdx: string | string[]
+}
+
+export const getServerSidePropsUserImage: GetServerSideProps = async (context) => {
+    const { userIdx } = context.query as any
+    const queryClient = new QueryClient()
+    await queryClient.prefetchQuery(['userImage', userIdx], () => useGetUserImage(userIdx))
+    return {
+        props: {
+            dehydratedState: dehydrate(queryClient),
+        },
+    }
+}
+export const getUserImage = (userIdx: PostData) => apiInstance.get(`/users/image/${userIdx}`, { withCredentials: true })
+
+const useGetUserImage = (userIdx: any) => {
+    const queryFn = () => getUserImage(userIdx)
+    return useQuery<AxiosResponse<any>, AxiosError>(['userImage', userIdx], queryFn)
+}
+
+export default useGetUserImage
