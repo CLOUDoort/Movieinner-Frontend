06494e1990af81b67478d386741aa5f9bdf06b87
JunSeok <ppu1234@naver.com>
2023-01-09 16:10:14 +0900

    Refactoring : header

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,91 +1,17 @@
-import Router from 'next/router'
-import { useEffect, useState } from 'react'
-import { useDispatch, useSelector } from 'react-redux'
-import { apiInstance } from '../../apis/setting'
-import { setToken, setNickname, setEmail, setIdx } from '../../store/reducers/logintokenSlice'
+import { useSelector } from 'react-redux'
 import { RootState } from '../../store/store'
-import { FirstHeaderDiv, HeaderContainer, SecondHearderDiv } from './Header.style'
-import { toast } from 'react-toastify'
-import HearderSearch from './HearderSearch'
-import HeaderNav from './HeaderNav'
-import { setSocialEmail } from '../../store/reducers/socialSlice'
+import { HeaderContainer } from './Header.style'
+import HearderSecondNav from './HearderSecondNav'
+import HeaderFirstNav from './HeaderFirstNav'
 
 const Header = () => {
     const loginToken = useSelector((state: RootState) => state.token.token)
     const nickname = useSelector((state: RootState) => state.nickname.nickname)
-    const dispatch = useDispatch()
-    const [loginToggle, setLoginToggle] = useState('로그인')
-
-    // login/logout UI 변경
-    useEffect(() => {
-        const getResponse = async () => {
-            if (loginToken) {
-                setLoginToggle('로그아웃')
-                const tokenPayload = await apiInstance.post('/auth/verify', { token: loginToken })
-                dispatch(setNickname(tokenPayload.data.payload.nickname))
-                dispatch(setSocialEmail(tokenPayload.data.payload.email))
-                dispatch(setEmail(tokenPayload.data.payload.email))
-                dispatch(setIdx(tokenPayload.data.payload.idx))
-            }
-        }
-        getResponse()
-    }, [loginToken])
-
-    // 브라우저에 refreshToken이 있으면 무조건 액세스 토큰이 재발급되니 UI는 로그아웃으로 변경
-    // 브라우저에 refreshToken이 있으면 액세스 토큰 재발급
-    useEffect(() => {
-        const refreshTokenCheck = async () => {
-            const refreshTokenResponse = await apiInstance.get('/auth')
-            const { isRefreshToken } = refreshTokenResponse.data
-            if (isRefreshToken) {
-                try {
-                    setLoginToggle('로그아웃')
-                    const response = await apiInstance.post('/auth/refresh')
-                    dispatch(setToken(response.data.accessToken))
-                    console.log('reload-silent-success')
-                } catch (e) {
-                    console.log(e.response)
-                }
-            } else {
-                console.log('none-refreshToken')
-                setLoginToggle('로그인')
-            }
-        }
-        refreshTokenCheck()
-    }, [])
-
-    // 클릭시 로그아웃이면 accessToken 없앰, 로그인UI로 변경
-    // 클릭시 로그인이면 accessToken이 없다는 것이니
-    const clickLogin = async () => {
-        if (loginToggle === '로그아웃') {
-            try {
-                const response = await apiInstance.post('/users/logout')
-                if (response.data.logout) {
-                    dispatch(setToken(''))
-                    dispatch(setNickname(''))
-                    dispatch(setSocialEmail(''))
-                    dispatch(setEmail(''))
-                    dispatch(setIdx(0))
-                    setLoginToggle('로그인')
-                    Router.replace('/')
-                    toast.success('로그아웃되었습니다!')
-                }
-            } catch (e) {
-                console.log(e.response)
-            }
-        } else Router.push('/login')
-    }
 
     return (
         <HeaderContainer>
-            <FirstHeaderDiv>
-                <button onClick={clickLogin}>{loginToggle}</button>
-                <button onClick={() => Router.push('/signup')}>회원가입</button>
-            </FirstHeaderDiv>
-            <SecondHearderDiv>
-                <HeaderNav nickname={nickname} />
-                <HearderSearch />
-            </SecondHearderDiv>
+            <HeaderFirstNav nickname={nickname} />
+            <HearderSecondNav loginToken={loginToken} />
         </HeaderContainer>
     )
 }
