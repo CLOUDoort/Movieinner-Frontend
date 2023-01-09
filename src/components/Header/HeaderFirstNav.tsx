2c39fa7a853661e5ade66ec63a8d878731c1c96a
JunSeok <ppu1234@naver.com>
2023-01-09 23:28:18 +0900

    Updated : header_nav

--- src/components/Header/HeaderFirstNav.tsx
+++ src/components/Header/HeaderFirstNav.tsx
@@ -22,27 +22,12 @@ const HeaderFirstNav = (props) => {
         <HearderNavFirstBox>
             <TitleDiv>Movie Inner</TitleDiv>
             <NavDiv>
-                <div>
-                    <Link href='/'>홈</Link>
-                    <div></div>
-                </div>
-                <div>
-                    <button onClick={clickModal}>카테고리</button>
-                    <div></div>
-                </div>
+                <Link href='/'>홈</Link>
+                <button onClick={clickModal}>카테고리</button>
                 {modal && <Category clickModal={clickModal} list={list} />}
-                <div>
-                    <Link href='/community/feed/1'>커뮤니티</Link>
-                    <div></div>
-                </div>
-                <div>
-                    <Link href='/theme'>테마</Link>
-                    <div></div>
-                </div>
-                <div>
-                    {nickname ? <Link href={`/user/${nickname}`}>마이페이지</Link> : <Link href='/login'>마이페이지</Link>}
-                    <div></div>
-                </div>
+                <Link href='/community/feed/1'>커뮤니티</Link>
+                <Link href='/theme'>테마</Link>
+                {nickname ? <Link href={`/user/${nickname}`}>마이페이지</Link> : <Link href='/login'>마이페이지</Link>}
             </NavDiv>
         </HearderNavFirstBox>
     )
