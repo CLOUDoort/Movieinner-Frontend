1335b71847fe0253f66528e7e5c2bae47600a921
JunSeok <ppu1234@naver.com>
2023-01-10 17:21:49 +0900

    Updated : header_noti

--- src/components/Header/HeaderNoti.tsx
+++ src/components/Header/HeaderNoti.tsx
@@ -1,21 +1,23 @@
+import { useState } from "react"
 import { IoIosNotificationsOutline } from "react-icons/io"
 import { useSelector } from "react-redux"
 import { toast } from "react-toastify"
 import { apiInstance } from "../../apis/setting"
 import { RootState } from "../../store/store"
 import useGetNotiData from "../react-query/NotificationData"
+import HeaderNotiModal from "./HeaderNotiModal"
 
 const HeaderNoti = () => {
     const userIdx = useSelector((state: RootState) => state.idx.idx)
     const commentNotiData = useGetNotiData(userIdx, "comment").data
     const replytNotiData = useGetNotiData(userIdx, "reply").data
-    const deleteNumber = commentNotiData?.data?.response[0]?.idx
+    const [modal, setModal] = useState(false)
     const deleteNoti = async () => {
         try {
             const response = await apiInstance.delete(`/community/notification`, {
                 data: {
                     userIdx: userIdx,
-                    notIdx: deleteNumber
+                    notIdx: 1
                 }
             })
             console.log('delete response', response.data)
@@ -28,7 +30,10 @@ const HeaderNoti = () => {
     console.log('notiheader', commentNotiData?.data)
     console.log('replyheader', replytNotiData?.data)
     return (
-        <IoIosNotificationsOutline size={40} />
+        <>
+            <IoIosNotificationsOutline onClick={() => setModal(!modal)} size={40} />
+            {modal ? <HeaderNotiModal /> : null}
+        </>
     )
 
 }
