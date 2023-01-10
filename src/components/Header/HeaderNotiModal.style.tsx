253d2a2de2afbfe38412db68e7c13fd6cbbc14ca
JunSeok <ppu1234@naver.com>
2023-01-10 17:21:57 +0900

    Added : header_noti_modal

---
+++ src/components/Header/HeaderNotiModal.style.tsx
@@ -0,0 +1,17 @@
+import styled from '@emotion/styled'
+
+export const HeaderNotiModalContainer = styled.div`
+    position: fixed;
+    top: 0;
+    left: 0;
+    width: 100%;
+    height: 100%;
+    background-color: rgba(0, 0, 0, 0.4);
+`
+
+export const HeaderNotiModalBox = styled.div`
+    position: absolute;
+    width: 500px;
+    height: 700px;
+    background-color: gray;
+`
---
+++ src/components/Header/HeaderNotiModal.tsx
@@ -0,0 +1,13 @@
+import { HeaderNotiModalBox, HeaderNotiModalContainer } from "./HeaderNotiModal.style"
+
+const HeaderNotiModal = () => {
+    return (
+        <HeaderNotiModalContainer>
+            <HeaderNotiModalBox>
+
+            </HeaderNotiModalBox>
+        </HeaderNotiModalContainer>
+    )
+}
+
+export default HeaderNotiModal
