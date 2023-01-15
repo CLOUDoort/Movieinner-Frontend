680c61f48b7676d9f2364094297c239bf5f03c2a
JunSeok <ppu1234@naver.com>
2023-01-15 13:22:12 +0900

    Updated : header_style

--- src/components/Header/Header.style.tsx
+++ src/components/Header/Header.style.tsx
@@ -2,12 +2,9 @@ import styled from '@emotion/styled'
 
 export const HeaderContainer = styled.header`
     width: 100%;
-    min-width: 400px;
     display: flex;
     justify-content: space-between;
-    overflow: hidden;
     border-bottom: 1px solid red;
     padding-bottom: 15px;
     padding: 15px;
-    
 `
