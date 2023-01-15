9b8c8e7e6f0a14258ebfcad8b01217a1965f0d9e
JunSeok <ppu1234@naver.com>
2023-01-15 15:17:31 +0900

    Added : Common_logout

---
+++ src/components/Common/CommonLogout.tsx
@@ -0,0 +1,25 @@
+import { useRouter } from "next/router"
+import { useDispatch } from "react-redux"
+import { toast } from "react-toastify"
+import { apiInstance } from "../../apis/setting"
+import { setEmail, setIdx, setNickname, setToken } from "../../store/reducers/logintokenSlice"
+import { setSocialEmail } from "../../store/reducers/socialSlice"
+
+export const logout = async () => {
+    const dispatch = useDispatch()
+    const router = useRouter()
+    try {
+        const response = await apiInstance.post('/users/logout')
+        if (response.data.logout) {
+            dispatch(setToken(''))
+            dispatch(setNickname(''))
+            dispatch(setSocialEmail(''))
+            dispatch(setEmail(''))
+            dispatch(setIdx(0))
+            router.replace('/')
+            toast.success('로그아웃되었습니다!')
+        }
+    } catch (e) {
+        console.log(e.response)
+    }
+}
