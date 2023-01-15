533b22fe516ecad4071495bd3cf8ec5aa18383b3
JunSeok <ppu1234@naver.com>
2023-01-15 14:29:54 +0900

    Updated : header_category_style

--- src/components/Header/HeaderCategory.style.tsx
+++ src/components/Header/HeaderCategory.style.tsx
@@ -15,22 +15,22 @@ type CategoryProps = {
 
 export const CategoryBox = styled.div<CategoryProps>`
     position: absolute;
-    top: 60px;
-    left: 250px;
+    top: 4rem;
+    left: 15.5rem;
     background-color: #ff0000;
-    width: 300px;
-    height: 250px;
+    width: 20rem;
+    height: 14.5rem;
     border-radius: 5px;
     opacity: ${(props) => props.transition ? 1 : 0};
-    transition: opacity 0.3s;
+    transition: opacity 0.4s;
 `
 
 export const CategoryList = styled.div`
     display: grid;
-    column-gap: 3px;
-    row-gap: 3px;
+    column-gap: 0.1rem;
+    row-gap: 0.1rem;
     grid-template-columns: 1fr 1fr 1fr;
-    margin-left: 20px;
-    margin-top: 15px;
-    font-size: 17px;
+    margin-left: 1.3rem;
+    margin-top: 1.2rem;
+    font-size: 1rem;
 `
