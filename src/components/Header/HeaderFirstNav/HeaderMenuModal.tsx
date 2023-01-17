ff26f87687d6dad6d1a9aafedd5848724abb22b5
JunSeok <ppu1234@naver.com>
2023-01-17 11:47:09 +0900

    Updated : hedaer_menu_modal

--- src/components/Header/HeaderFirstNav/HeaderMenuModal.tsx
+++ src/components/Header/HeaderFirstNav/HeaderMenuModal.tsx
@@ -11,7 +11,7 @@ const HeaderMenuModal = (props) => {
             <HeaderMenuModalBox height={height}>
                 <div>
                     <Link href='/'>홈</Link>
-                    <Link href='/community/feed/1'>커뮤니티</Link>
+                    <Link href='/community/feed'>커뮤니티</Link>
                     <Link href='/theme'>테마</Link>
                     {nickname ? <Link href={`/user/${nickname}`}>마이페이지</Link> : <Link href='/login'>마이페이지</Link>}
                 </div>
