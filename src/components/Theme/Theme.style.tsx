e59741c0d0d1fd08128fa53686ba7ac546f9121a
JunSeok <ppu1234@naver.com>
2023-01-17 14:27:48 +0900

    Updated : theme_modal_style

--- src/components/Theme/Theme.style.tsx
+++ src/components/Theme/Theme.style.tsx
@@ -9,6 +9,11 @@ export const ThemeContainer = styled.div`
     > p {
         margin-top: 1.25rem;
         margin-bottom: 1.25rem;
+        font-size: 1.3rem;
+        font-weight: bold;
+    }
+    @media screen and (max-width: 37.5em) {
+        > p {font-size: 0.9rem;}
     }
 `
 
@@ -56,81 +61,5 @@ export const ThemeItemContainer = styled.div`
         left: 1.875rem;
         font-size: 1.25rem;
         mix-blend-mode: difference;
-    }
-`
-
-export const ThemeModalBox = styled.div`
-    position: fixed;
-    top: 0;
-    left: 0;
-    width: 100%;
-    height: 100%;
-    background-color: rgba(0, 0, 0, 0.4);
-    display: flex;
-    justify-content: center;
-    align-items: center;
-    z-index: 2000;
-`
-
-export const ThemeModalContainer = styled.div`
-    position: absolute;
-    padding: 0.625rem;
-    text-align: center;
-    background-color: #282727;
-    border-radius: 0.625rem;
-    border: 1px solid rgba(0, 0, 0, 0.3);
-    box-shadow: 0 2px 3px 0 rgba(34, 36, 38, 0.15);
-    > p {
-        margin-bottom: 1.25rem;
-        font-size: 1.563rem;
-    }
-`
-export const ThemeModalItemContainer = styled.div`
-    display: grid;
-    grid-template-columns: 1fr 1fr 1fr;
-    row-gap: 1.875rem;
-`
-
-export const ThemeModalItem = styled.div`
-    width: 12.5rem;
-    height: 16.875rem;
-    position: relative;
-    > span {
-        object-fit: contain;
-        cursor: pointer;
-        border-radius: 5px;
-    }
-`
-export const ThemeModalContent = styled.div`
-    display: flex;
-    flex-direction: column;
-    height: 2.5rem;
-    > p {
-        margin-bottom: 5px;
-    }
-`
-
-type ThemeLikeBtnProps = {
-    like: boolean
-}
-
-export const ThemeLikeBtn = styled.div<ThemeLikeBtnProps>`
-    position: absolute;
-    top: 0.625rem;
-    right: 3.75rem;
-    cursor: pointer;
-    > svg {
-        border-radius: 5px;
-        color: ${(props) => (props.like ? 'red' : 'white')};
-    }
-`
-export const ThemeCloseBtn = styled.div`
-    position: absolute;
-    top: 0.625rem;
-    right: 0.9rem;
-    cursor: pointer;
-    > svg {
-        color: #fefefe;
-        border-radius: 5px;
     }
 `
---
+++ src/components/Theme/ThemeModal.style.tsx
@@ -0,0 +1,86 @@
+import styled from '@emotion/styled'
+
+export const ThemeModalBox = styled.div`
+    position: fixed;
+    top: 0;
+    left: 0;
+    width: 100%;
+    height: 100%;
+    background-color: rgba(0, 0, 0, 0.4);
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    z-index: 2000;
+`
+
+export const ThemeModalContainer = styled.div`
+    position: absolute;
+    padding: 0.625rem;
+    text-align: center;
+    background-color: #1b1b1b;
+    border-radius: 1.375rem;
+    border: 1px solid rgba(0, 0, 0, 0.4);
+    > p {
+        margin-top: 0.5rem;
+        margin-bottom: 1.5rem;
+        font-size: 1.7rem;
+    }
+`
+export const ThemeModalItemContainer = styled.div`
+    display: grid;
+    grid-template-columns: 1fr 1fr 1fr;
+    row-gap: 1.875rem;
+    padding: 0.5rem;
+`
+
+export const ThemeModalItem = styled.div`
+    width: 12.5rem;
+    height: 16rem;
+    @media screen and (max-width: 50em) {
+        width: 10rem;
+        height: 15rem;
+    }
+    @media screen and (max-width: 37.5em) {
+        width: 7rem;
+        height: 12rem;
+    }
+    position: relative;
+    > span {
+        object-fit: contain;
+        cursor: pointer;
+        border-radius: 5px;
+    }
+`
+export const ThemeModalContent = styled.div`
+    display: flex;
+    flex-direction: column;
+    height: 2.5rem;
+    > p {
+        margin-bottom: 5px;
+    }
+`
+
+type ThemeLikeBtnProps = {
+    like: boolean
+}
+
+export const ThemeLikeBtn = styled.div<ThemeLikeBtnProps>`
+    position: absolute;
+    top: 0.625rem;
+    right: 3.75rem;
+    cursor: pointer;
+    > svg {
+        border-radius: 5px;
+        color: ${(props) => (props.like ? 'red' : 'white')};
+    }
+`
+export const ThemeCloseBtn = styled.div`
+    position: absolute;
+    top: 0.625rem;
+    right: 0.9rem;
+    cursor: pointer;
+    > svg {
+        color: #fefefe;
+        border-radius: 5px;
+    }
+`
