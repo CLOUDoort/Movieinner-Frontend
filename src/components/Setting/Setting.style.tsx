d815c5017a5bd129ddc0827976963152867abd91
JunSeok <ppu1234@naver.com>
2023-01-17 16:22:02 +0900

    Updated : setting_style

--- src/components/Setting/Setting.style.tsx
+++ src/components/Setting/Setting.style.tsx
@@ -14,26 +14,8 @@ export const SettingBox = styled.div`
     width: 800px;
     flex-direction: column;
     margin-top: 10px;
-    > div {
+    > * {
         margin-bottom: 30px;
-    }
-    >:nth-of-type(2) {
-        display: flex;
-        justify-content: center;
-        flex-shrink: 0;
-        align-items: center;
-        >span {
-            margin-right: 10px;
-            width: 120px;
-            text-align: center;
-        }
-        >:last-of-type {
-            background-color: red;
-            font-size: 15px;
-            padding: 3px 6px;
-            border-radius: 5px;
-            cursor: pointer;
-        }
     }
 `
 
@@ -55,30 +37,3 @@ export const SettingDeleteUser = styled.div`
     }
 `
 
-export const SettingModifyImage = styled.div`
-    display: flex;
-    flex-direction: column;
-    justify-content: center;
-    align-items: center;
-    border-right: 1px solid gray;
-    padding-right: 15px;
-    > span {
-        border-radius: 50%;
-        margin: 20px;
-        object-fit: scale-down;
-    }
-    > label {
-        background-color: #05ce05;
-        font-size: 13px;
-        padding: 3px 6px;
-        width: 85px;
-        border-radius: 5px;
-        cursor: pointer;
-        margin-bottom: 5px;
-        margin-top: 5px;
-        text-align: center;
-    }
-    >:last-of-type {
-        background-color: red;
-    }
-`
