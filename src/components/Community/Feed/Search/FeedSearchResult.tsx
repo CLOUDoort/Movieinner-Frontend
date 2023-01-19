b680c9d63be393b0c288ff8afd0d360fec733452
JunSeok <ppu1234@naver.com>
2023-01-20 02:02:42 +0900

    Updated : pagination

--- src/components/Community/Feed/Search/FeedSearchResult.tsx
+++ src/components/Community/Feed/Search/FeedSearchResult.tsx
@@ -3,12 +3,9 @@ import { useState } from 'react'
 import { useSelector } from 'react-redux'
 import { toast } from 'react-toastify'
 import useGetFeedSearchData from '../../../../apis/CommunityData/FeedSearchData'
-import useGetHitFeed from '../../../../apis/CommunityData/HitFeedData'
 import { RootState } from '../../../../store/store'
-import LoadingLogo from '../../../Common/Loading/LoadingLogo'
 import { FeedContainer } from '../Feed.style'
 import FeedPagination from '../FeedPagination'
-import FeedRanking from '../FeedRanking'
 import FeedRemote from '../FeedRemote'
 import FeedSearchList from './FeedSearchList'
 
@@ -16,47 +13,20 @@ const FeedSearchResult = () => {
     const accessToken = useSelector((state: RootState) => state.token.token)
     const router = useRouter()
     const { type, search, page } = router.query
-    const hitData = useGetHitFeed().data
     const { data } = useGetFeedSearchData(type, search, page)
     const [currentPage, setPageValue] = useState(1)
-
     const clickWrite = () => {
-        if (accessToken) {
-            router.push('/community/write')
-        } else {
-            toast.error('로그인이 필요합니다!')
-        }
-    }
-    const handlePaginationChange = (value) => {
-        try {
-            setPageValue(value)
-            router.push({
-                pathname: `/community/feed/search/${type}`,
-                query: {
-                    search: search,
-                    page: page,
-                },
-            })
-        } catch (e) {
-            console.error(e.response)
-        }
+        if (accessToken) router.push('/community/write')
+        else toast.error('로그인이 필요합니다!')
     }
 
     return (
-        <>
-            {data ? (
-                <FeedContainer>
-                    <>
-                        <FeedRanking hit={hitData?.data?.top5Contents} />
-                        <FeedSearchList searchList={data?.data.searchResult.responseContents} />
-                        <FeedRemote clickWrite={clickWrite} />
-                        <FeedPagination currentPage={currentPage} setCurrentPage={setPageValue} maxPage={data?.data?.contents?.totalPage} />
-                    </>
-                </FeedContainer>
-            ) : (
-                <LoadingLogo />
-            )}
-        </>
+        <FeedContainer>
+            <p>검색 : {search}</p>
+            <FeedSearchList searchList={data?.data.searchResult.responseContents} />
+            <FeedRemote clickWrite={clickWrite} />
+            <FeedPagination currentPage={currentPage} setCurrentPage={setPageValue} maxPage={data?.data?.searchResult?.totalPage} />
+        </FeedContainer>
     )
 }
 
