5fe0cc60961a149fe8a8849809404adb19996da5
JunSeok <ppu1234@naver.com>
2023-01-20 00:48:13 +0900

    Deleted : hr

--- src/components/MovieInfo/MovieLike.style.tsx
+++ src/components/MovieInfo/MovieLike.style.tsx
@@ -16,15 +16,3 @@ export const MovieLikeBtn = styled.div<MovieLikeBtnProps>`
         margin-right: 0.625rem;
     }
 `
-
-export const HorizontalRule = styled.hr`
-    width: 50rem;
-    height: 1px;
-    @media screen and (max-width: 53.13em) {
-        width: 35rem;
-    }
-    @media screen and (max-width: 37.5em) {
-        width: 20rem;
-    }
-`
-
