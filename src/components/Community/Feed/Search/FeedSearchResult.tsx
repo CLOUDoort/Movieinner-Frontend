60b99ed8190afdf0dda72b0755d96ac168114745
JunSeok <ppu1234@naver.com>
2023-01-20 02:14:41 +0900

    Updated : loading_logo

--- src/components/Community/Feed/Search/FeedSearchResult.tsx
+++ src/components/Community/Feed/Search/FeedSearchResult.tsx
@@ -4,6 +4,7 @@ import { useSelector } from 'react-redux'
 import { toast } from 'react-toastify'
 import useGetFeedSearchData from '../../../../apis/CommunityData/FeedSearchData'
 import { RootState } from '../../../../store/store'
+import LoadingLogo from '../../../Common/Loading/LoadingLogo'
 import { FeedContainer } from '../Feed.style'
 import FeedPagination from '../FeedPagination'
 import FeedRemote from '../FeedRemote'
@@ -14,19 +15,21 @@ const FeedSearchResult = () => {
     const router = useRouter()
     const { type, search } = router.query
     const [currentPage, setPageValue] = useState(1)
-    const { data } = useGetFeedSearchData(type, search, currentPage)
+    const { data, isLoading } = useGetFeedSearchData(type, search, currentPage)
     const clickWrite = () => {
         if (accessToken) router.push('/community/write')
         else toast.error('로그인이 필요합니다!')
     }
 
     return (
-        <FeedContainer>
-            <p>검색 : {search}</p>
-            <FeedSearchList searchList={data?.data.searchResult.responseContents} />
-            <FeedRemote clickWrite={clickWrite} />
-            <FeedPagination currentPage={currentPage} setCurrentPage={setPageValue} maxPage={data?.data?.searchResult?.totalPage} />
-        </FeedContainer>
+        <>
+            {!isLoading ? <FeedContainer>
+                <p>검색 : {search}</p>
+                <FeedSearchList searchList={data?.data.searchResult.responseContents} />
+                <FeedRemote clickWrite={clickWrite} />
+                <FeedPagination currentPage={currentPage} setCurrentPage={setPageValue} maxPage={data?.data?.searchResult?.totalPage} />
+            </FeedContainer> : <LoadingLogo />}
+        </>
     )
 }
 
