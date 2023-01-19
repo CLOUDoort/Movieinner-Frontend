45980e9ce265d439b51666db9a8e23a6d76ddf98
JunSeok <ppu1234@naver.com>
2023-01-20 02:56:37 +0900

    Added : editor_style

--- src/components/Community/Write/Write.style.tsx
+++ src/components/Community/Write/Write.style.tsx
@@ -7,30 +7,69 @@ export const WriteContainer = styled.div`
     align-items: center;
     padding: 1.25rem;
 `
+
 export const WriteTitle = styled.input`
-    background-color: #232428;
-    width: 45.313rem;
+    background-color: black;
+    width: 50rem;
+    height: 4rem;
+    @media screen and (max-width: 53.13em) {
+        width: 35rem;
+    }
+    @media screen and (max-width: 37.5em) {
+        width: 23rem;
+    }
     color: #fff;
-    height: 60px;
-    border-radius: 2px;
+    border: 1px solid #3e3e3ee4;
+    border-bottom: none;
     font-size: 1.25rem;
-    border: 1px solid #303238;
-    padding: 18px 25px;
+    padding: 1rem 1.5rem;
     outline: none;
 `
+export const WriteEditor = styled.div`
+    width: 50rem !important;
+    border: none;
+    @media screen and (max-width: 53.13em) {
+        width: 35rem !important;
+    }
+    @media screen and (max-width: 37.5em) {
+        width: 23rem !important;
+    }
+    overflow: hidden;
+    overflow-y: scroll;
+    &::-webkit-scrollbar {
+      width: 1px;
+      height: 0.5rem;
+    }
+    &::-webkit-scrollbar-thumb {
+     border-radius: 2px;
+      background: black;
+    }
+`
 export const WriteBtn = styled.div`
-    width: 45rem;
-    height: 55px;
-    background-color: #232428;
+    width: 50rem;
+    @media screen and (max-width: 53.13em) {
+        width: 35rem;
+    }
+    @media screen and (max-width: 37.5em) {
+        width: 23rem;
+    }
+    height: 4rem;
     display: flex;
     justify-content: space-between;
+    border: 1px solid #3e3e3ee4;
+    border-top: none;
     border-bottom-left-radius: 3px;
     border-bottom-right-radius: 3px;
     :last-child {
-        background-color: #b7cbc5;
+        background-color: #000000;
         padding: 0.625rem 1.25rem;
     }
     > button {
-        font-size: 1.25rem;
+        border: none;
+        padding: 0.3rem 1rem;
+        font-size: 1rem;
+        :hover {
+            background-color: green;
+        }
     }
 `
