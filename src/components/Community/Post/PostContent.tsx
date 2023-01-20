c0f6a34a70a5a5ab7304d8337f6e83da5a9e162c
JunSeok <ppu1234@naver.com>
2023-01-20 22:54:52 +0900

    Added : post_content_authorization

--- src/components/Community/Post/PostContent.tsx
+++ src/components/Community/Post/PostContent.tsx
@@ -1,11 +1,14 @@
 import { Avatar } from '@mui/material'
 import router from 'next/router'
 import { useState } from 'react'
+import { useSelector } from 'react-redux'
+import { RootState } from '../../../store/store'
 import { PostContentArea, PostInfo } from './Post.style'
 import PostConfirmModal from './PostConfirmModal'
 
 const PostContent = (props) => {
     const { data, idx } = props
+    const nickname = useSelector((state: RootState) => state.nickname.nickname)
     const [confirmModal, setConfirmModal] = useState(false)
     const clickCheck = () => setConfirmModal(!confirmModal)
     console.log('postData', data)
@@ -19,13 +22,13 @@ const PostContent = (props) => {
                         <div>{data?.nickname}</div>
                         <div>{data?.created_at}</div>
                     </div>
-                    <div>
+                    {nickname == data?.nickname && <div>
                         <div onClick={() => {
                             router.push(`/community/modify/${idx}`)
                         }}>수정</div>
                         <div onClick={clickCheck}>삭제</div>
                         {confirmModal ? <PostConfirmModal idx={idx} confirmModal={confirmModal} clickCheck={clickCheck} /> : null}
-                    </div>
+                    </div>}
                 </div>
             </PostInfo>
             <div dangerouslySetInnerHTML={{ __html: data.content }} />
