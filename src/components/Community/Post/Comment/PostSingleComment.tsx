9394da2085473d026927bdb1d1c0436c0780870e
JunSeok <ppu1234@naver.com>
2023-01-20 23:19:48 +0900

    Updated : etc

--- src/components/Community/Post/Comment/PostSingleComment.tsx
+++ src/components/Community/Post/Comment/PostSingleComment.tsx
@@ -5,14 +5,18 @@ import PostReplyWrite from './PostReplyWrite'
 import { apiInstance } from '../../../../apis/setting'
 import { toast } from 'react-toastify'
 import { CommentWrite } from './PostComment.style'
+import { useSelector } from 'react-redux'
+import { RootState } from '../../../../store/store'
 
 const PostSingleComment = (props) => {
     const [openReply, setOpenReply] = useState(false)
-    const { comment, accessToken, idx, nickname, refreshFunction, clickView, userIdx, image } = props
+    const { comment, accessToken, idx, refreshFunction, clickView, userIdx, image } = props
     const [modify, setModify] = useState(false)
+    const nickname = useSelector((state: RootState) => state.nickname.nickname)
     const [modifyComment, setModifyComment] = useState(comment.comment)
     const deleteComment = '삭제된 댓글입니다.'
     const [deleteState, setDeleteState] = useState(false)
+
 
     console.log('comment', comment)
     const clickReply = () => {
@@ -51,13 +55,16 @@ const PostSingleComment = (props) => {
                                 <div>
                                     <Avatar alt='User Image' src={image ? image : '/blank.png'} />
                                     <div>
-                                        <div>{nickname}</div>
+                                        <div>{comment.nickname}</div>
                                         <div>{modifyComment}</div>
                                     </div>
                                 </div>
                                 <div>
-                                    <button onClick={handleModify}>수정하기</button>
-                                    <button onClick={clickDelete}>삭제하기</button>
+                                    {
+                                        nickname == comment.nickname && <><button onClick={handleModify}>수정</button>
+                                            <button onClick={clickDelete}>삭제</button></>
+                                    }
+
                                 </div>
                             </CommentContent>
                         </CommentUser>
@@ -70,8 +77,8 @@ const PostSingleComment = (props) => {
                 <CommentWrite>
                     <textarea value={modifyComment} onChange={handleChange} placeholder='댓글 작성해주세요!'></textarea>
                     <div>
-                        <button onClick={handleModify}>취소하기</button>
-                        <button onClick={clickModify}>수정하기</button>
+                        <button onClick={handleModify}>취소</button>
+                        <button onClick={clickModify}>수정</button>
                     </div>
                 </CommentWrite>
             )}
@@ -82,9 +89,8 @@ const PostSingleComment = (props) => {
                     reply={comment?.idx}
                     accessToken={accessToken}
                     contentIdx={idx}
-                    nickname={nickname}
+                    nickname={comment.nickname}
                     clickView={clickView}
-                    userIdx={userIdx}
                 />
             )}
         </>
