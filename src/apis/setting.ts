38b7e54dd66edab9e23cc1cadf5b1381cd6f0a72
JunSeok <ppu1234@naver.com>
2023-01-24 00:36:04 +0900

    Update : server

--- src/apis/setting.ts
+++ src/apis/setting.ts
@@ -1,3 +1,3 @@
 import axios from 'axios'
 
-export const apiInstance = axios.create({ baseURL: 'https://api.movie-inner.click', withCredentials: true })
+export const apiInstance = axios.create({ baseURL: 'http://localhost:3714', withCredentials: true })
